07917b01b73539b1ee2a54d571ce7fd298a7d4a3
bachar-alhamada <bachar.alhamada76@gmail.com>
2023-12-16 03:27:42 -0600

    finish fatching the random people using API &posting section

--- assets/script/home.js
+++ assets/script/home.js
@@ -1,21 +1,52 @@
-    fetchProfiles();
+const suggestionsContainer = document.querySelector('.suggestions-column');
 
-function fetchProfiles() {
-    fetch('https://randomuser.me/api/?results=10')
-        .then(response => response.json())
-        .then(data => displayProfiles(data.results))
-        .catch(err => console.error(err));
+// Function to create a user card
+function createUserCard(user) {
+    // Create elements
+    const userCard = document.createElement('div');
+    userCard.className = 'user-card';
+
+    const profileImage = document.createElement('img');
+    profileImage.src = user.picture.medium; // The src will be the URL from the API data
+    profileImage.alt = `${user.name.first} ${user.name.last}`;
+    profileImage.className = 'profile-pic';
+
+    const userName = document.createElement('div');
+    userName.className = 'user-name';
+    userName.textContent = `${user.name.first} ${user.name.last}`;
+
+    const userCity = document.createElement('div');
+    userCity.className = 'user-city';
+    userCity.textContent = user.location.city; // The city text
+
+    const userOccupation = document.createElement('div');
+    userOccupation.className = 'user-occupation';
+    userOccupation.textContent = user.occupation; // The occupation text
+
+    const addFriendIcon = document.createElement('i');
+    addFriendIcon.className = 'fas fa-user-plus add-friend';
+    addFriendIcon.title = 'Add Friend';
+
+    // Append elements to the user card in the right order
+    userCard.appendChild(profileImage);
+    userCard.appendChild(userName);
+    userCard.appendChild(userCity); // Append the city element right after the user name
+    userCard.appendChild(userOccupation);
+    userCard.appendChild(addFriendIcon); // Append the add friend icon
+
+    return userCard;
 }
 
-function displayProfiles(profiles) {
-    const suggestionsColumn = document.querySelector('.suggestions-column');
-    profiles.forEach(profile => {
-        const profileDiv = document.createElement('div');
-        profileDiv.innerHTML = `
-            <img src="${profile.picture.medium}" alt="${profile.name.first}">
-            <p>${profile.name.first} ${profile.name.last}</p>
-            <p>${profile.location.city}</p>
-        `;
-        suggestionsColumn.appendChild(profileDiv);
+// Example of using fetched API data
+fetch('https://randomuser.me/api/?results=11') // Fetches 3 random users
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(user => {
+            // Assuming the API returns an object with 'name', 'picture', and 'occupation' properties
+            const userCard = createUserCard(user);
+            suggestionsContainer.appendChild(userCard);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching user data:', error);
     });
-}
